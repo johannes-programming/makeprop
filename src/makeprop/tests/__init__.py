dba61dc58fa85cc9940e5cc4d3f989c6543504f9
Johannes <johannes.programming@gmail.com>
2026-02-11 01:36:45 +0100

    Version 1.0.2

--- src/makeprop/tests/__init__.py
+++ src/makeprop/tests/__init__.py
@@ -4,8 +4,8 @@ __all__ = ["test"]
 
 
 def test() -> unittest.TextTestRunner:
-    loader: unittest.TestLoader = unittest.TestLoader()
-    tests: unittest.TestSuite = loader.discover(start_dir="makeprop.tests")
-    runner: unittest.TextTestRunner = unittest.TextTestRunner()
-    result: unittest.TextTestRunner = runner.run(tests)
-    return result
+    loader: unittest.TestLoader
+    tests: unittest.TestSuite
+    loader = unittest.TestLoader()
+    tests = loader.discover(start_dir="makeprop.tests")
+    return unittest.TextTestRunner().run(tests)
