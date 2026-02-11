30f01bbbd4a200fe311dd2485464bcdb1f691d26
Johannes <johannes.programming@gmail.com>
2026-02-11 02:00:50 +0100

    Version 1.0.5

--- src/makeprop/core/__init__.py
+++ src/makeprop/core/__init__.py
@@ -1,9 +1,9 @@
 import dataclasses
 from typing import *
 
-from overloadable import overloadable
-
 __all__ = ["makeprop"]
+
+DEFAULT = object()
 
 
 @dataclasses.dataclass
@@ -11,36 +11,6 @@ class makeprop:
     var: Optional[str] = None
     hasdeleter: bool = False
     deletervalue: object = None
-
-    @overloadable
-    def __init__(self: Self, *args: Any, **kwargs: Any) -> bool:
-        "This function works as dispatcher."
-        return "delete" in kwargs.keys()
-
-    @__init__.overload(False)
-    def __init__(self: Self, var: Optional[str] = None) -> None:
-        "This magic method sets up the current instance."
-        return self.__init_1(var)
-
-    @__init__.overload(True)
-    def __init__(self: Self, var: Optional[str] = None, *, delete: object) -> None:
-        "This magic method sets up the current instance."
-        return self.__init_1(var, hasdeleter=True, deletervalue=delete)
-
-    def __init_1(
-        self,
-        var: Optional[str] = None,
-        /,
-        hasdeleter: bool = False,
-        deletervalue: object = None,
-    ) -> None:
-        "This method is the common ending of both versions of __init__."
-        if var is None:
-            self.var = None
-        else:
-            self.var = str(var)
-        self.hasdeleter = hasdeleter
-        self.deletervalue = deletervalue
 
     def __call__(self: Self, func: Callable) -> property:
         "This magic method implements calling the current instance."
@@ -52,24 +22,44 @@ class makeprop:
         else:
             var = self.var
         deletervalue = self.deletervalue
-
         kwargs = dict(doc=func.__doc__)
 
-        def fget(_self):
+        def fget(_self: Self) -> Any:
             return getattr(_self, var)
 
         kwargs["fget"] = fget
 
-        def fset(_self, value) -> None:
+        def fset(_self: Self, value: Any) -> None:
             setattr(_self, var, func(_self, value))
 
         kwargs["fset"] = fset
 
         if self.hasdeleter:
 
-            def fdel(_self) -> None:
+            def fdel(_self: Self) -> None:
                 setattr(_self, var, func(_self, deletervalue))
 
             kwargs["fdel"] = fdel
 
         return property(**kwargs)
+
+    @overload
+    def __init__(self: Self, var: Optional[str] = None) -> None:
+        "This magic method sets up the current instance."
+        ...
+
+    @overload
+    def __init__(self: Self, var: Optional[str] = None, *, delete: object) -> None:
+        "This magic method sets up the current instance."
+        ...
+
+    def __init__(
+        self: Self,
+        var: Optional[str] = None,
+        *,
+        delete: object = DEFAULT,
+    ) -> None:
+        "This magic method sets up the current instance."
+        self.var = None if (var is None) else str(var)
+        self.hasdeleter = delete is not DEFAULT
+        self.deletervalue = delete if self.hasdeleter else None
